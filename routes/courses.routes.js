ceef4ce5db065eb2ad0f80225c622072a55e7647
prageeth dananjaya <e19431@eng.pdn.ac.lk>
2025-12-17 07:25:21 +0530

    add authentication middleware to course route

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const coursesController = require("../controllers/courses.controller");
+const auth = require("../middleware/auth");
 
-router.get("/", coursesController.getAllCourses);
-router.post("/", coursesController.createCourse);
+router.get("/", auth, coursesController.getAllCourses);
+router.post("/", auth, coursesController.createCourse);
 
 module.exports = router;
