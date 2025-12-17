c79c762d89a8fbddf4ac53a892dd4cad6081da8c
prageeth dananjaya <e19431@eng.pdn.ac.lk>
2025-12-17 08:35:19 +0530

    add rate limit to the application

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const routes = require("./routes");
 const connectDB = require("./config/db");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
+const rateLimit = require("express-rate-limit");
 
 app.use(express.json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 50,
+});
+
+app.use(limiter);
 app.use("/api", routes);
 
 const startServer = async () => {
